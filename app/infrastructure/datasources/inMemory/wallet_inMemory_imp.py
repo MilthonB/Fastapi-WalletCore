ed0fa1d40184434b24d6ec0b5034202e69e3e416
Milthon <milthonbor@gmail.com>
2025-12-30 20:15:46 -0700

    added logic in datasources wallet

--- app/infrastructure/datasources/inMemory/wallet_inMemory_imp.py
+++ app/infrastructure/datasources/inMemory/wallet_inMemory_imp.py
@@ -1,64 +1,67 @@
-from typing import List
+from typing import List, Dict
+from dataclasses import replace
 from uuid import UUID
-from decimal import Decimal
+#from decimal import Decimal
 from datetime import datetime
 
 from domain.entities.wallet_entity import WalletEntity
 from domain.value_objects.currency_value_object import CurrencyValueObject
-from domain.value_objects.money_value_object import MoneyValueObject
+#from domain.value_objects.money_value_object import MoneyValueObject
 from domain.contracts.repository.wallet_repository_domain import WalletRepository
 
 
 
 class WalletRepositoryImp(WalletRepository):
+
+    def __init__(self) -> None:
+        self.data: Dict[str, WalletEntity] = {}
+
     def get_wallet_by_id(self, wallet_id: UUID) -> WalletEntity:
-        return WalletEntity(
-            id=wallet_id,
-            currency=CurrencyValueObject(code="USD"),
-            balance=MoneyValueObject(amount=Decimal("10.0"), currency=CurrencyValueObject(code="USD")),
-            is_active=True,
-            created_at=datetime.now(),
-        )
+
+        wallet: WalletEntity|None= self.data.get(str(wallet_id))
+        if not wallet:
+            raise Exception("Wallet not found")
+
+        return wallet
+
     def create_wallet(self, wallet: WalletEntity) -> WalletEntity:
-         return WalletEntity(
-            id=wallet.id,
-            currency=wallet.currency,
-            balance=MoneyValueObject(amount=Decimal("10.0"), currency=CurrencyValueObject(code="USD")),
-            is_active=True,
-            created_at=datetime.now(),
-        )
+
+        if not self.data.get(str(wallet.id)):
+            raise Exception("wallet not found")
+
+        if self.data.get(str(wallet.id)):
+            raise Exception("Wallet already exists")
+
+        self.data[str(wallet.id)] = wallet
+        return wallet
+
     def update_wallet_by_id(self, wallet_id: UUID) -> WalletEntity:
-        return WalletEntity(
-            id=wallet_id,
+        wallet: WalletEntity | None =  self.data.get(str(wallet_id))
+
+        if not wallet:
+            raise Exception("Wallet not found")
+
+        wallet_new: WalletEntity = replace(
+            wallet,
             currency=CurrencyValueObject(code="USD"),
-            balance=MoneyValueObject(amount=Decimal("20.0"), currency=CurrencyValueObject(code="USD")),
-            is_active=True,
-            created_at=datetime.now(),
-            updated_at=datetime.now(),
+            updated_at=datetime.now()
         )
+
+
+        self.data[str(wallet_id)] = wallet_new
+
+        return wallet_new
+
     def delete_wallet_by_id(self, wallet_id: UUID) -> WalletEntity:
-        return WalletEntity(
-            id=wallet_id,
-            currency=CurrencyValueObject(code="USD"),
-            balance=MoneyValueObject(amount=Decimal("0.0"), currency=CurrencyValueObject(code="USD")),
-            is_active=False,
-            created_at=datetime.now(),
-            updated_at=datetime.now(),
-        )
+
+        wallet: WalletEntity | None = self.data.get(str(wallet_id))
+
+        if not wallet:
+            raise Exception("Wallet not found")
+        
+        del self.data[str(wallet_id)]
+
+        return wallet
+       
     def list_all_wallets(self) -> List[WalletEntity]:
-        return [
-            WalletEntity(
-                id=UUID("123e4567-e89b-12d3-a456-426614174000"),
-                currency=CurrencyValueObject(code="USD"),
-                balance=MoneyValueObject(amount=Decimal("10.0"), currency=CurrencyValueObject(code="USD")),
-                is_active=True,
-                created_at=datetime.now(),
-            ),
-            WalletEntity(
-                id=UUID("123e4567-e89b-12d3-a456-426614174001"),
-                currency=CurrencyValueObject(code="EUR"),
-                balance=MoneyValueObject(amount=Decimal("15.0"), currency=CurrencyValueObject(code="EUR")),
-                is_active=True,
-                created_at=datetime.now(),
-            ),
-        ]
+        return List(self.data.values())
